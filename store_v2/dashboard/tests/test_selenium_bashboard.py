4d9e63f1f732b226db319336b949796d283b852a
мансур зурабов <asl.zurabov@gmail.com>
2022-03-10 17:46:56 +0300

    vscode setup and new app "inventory"

--- store_v2/dashboard/tests/test_selenium_bashboard.py
+++ store_v2/dashboard/tests/test_selenium_bashboard.py
@@ -1,22 +1,23 @@
-import pytest 
+import pytest
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User
 
 # @pytest.mark.selenium
 # def test_create_new_admin_user(create_admin_user):
 #     assert create_admin_user.__str__() == 'admin'
 
+
 @pytest.mark.selenium
-def test_dashboard_admin_login(live_server,db_fixture_setup,edge_browser_instance):
+def test_dashboard_admin_login(
+    live_server, db_fixture_setup, edge_browser_instance
+):
     browser = edge_browser_instance
-    i = User.objects.get(id=1)
-    print(i.password)
 
-    browser.get(('%s%s' % (live_server.url, '/admin/login/')))
+    browser.get(("%s%s" % (live_server.url, "/admin/login/")))
 
-    user_name = browser.find_element(By.NAME, 'username')
-    user_password = browser.find_element(By.NAME, 'password')
+    user_name = browser.find_element(By.NAME, "username")
+    user_password = browser.find_element(By.NAME, "password")
     submit = browser.find_element(By.XPATH, '//input[@value="Log in"]')
     user_name.send_keys("admin")
     user_password.send_keys("test12345")
--- store_v2/tests/fixtures.py
+++ store_v2/tests/fixtures.py
@@ -1,18 +1,22 @@
-import pytest 
+import pytest
 from django.core.management import call_command
+
 
 @pytest.fixture
 def create_admin_user(django_user_model):
     """
     Return admin user
     """
-    return django_user_model.objects.create_superuser('admin', 'a@a.com', 'test12345')
+    return django_user_model.objects.create_superuser(
+        "admin", "a@a.com", "test12345"
+    )
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def db_fixture_setup(django_db_setup, django_db_blocker):
     """
     Load DB data fixtures
     """
-    print('adssss')
+    print("adssss")
     with django_db_blocker.unblock():
         call_command("loaddata", "db_admin_fixture.json")
