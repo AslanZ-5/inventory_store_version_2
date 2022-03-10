e6cba1138d11e842340edd434c83f8a5eb0a8f9c
мансур зурабов <asl.zurabov@gmail.com>
2022-03-10 19:06:07 +0300

    creating tests for inventory model Category (before it was created) --> with factories and dbfixtures

---
+++ store_v2/tests/factories.py
@@ -0,0 +1,15 @@
+import factory
+import pytest
+from faker import Faker
+from pytest_factoryboy import register
+
+fake = Faker()
+from store_v2.inventory import models
+
+
+class CategoryFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Category
+
+    name = fake.lexify(text="cat_name_??????")
+    slug = fake.lexify(text="cat_slug_??????")
