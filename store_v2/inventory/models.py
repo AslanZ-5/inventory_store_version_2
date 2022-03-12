87b7cc365fec45733c89c1a1c3593bb62720629e
мансур зурабов <asl.zurabov@gmail.com>
2022-03-12 19:21:13 +0300

    Media model has been created

--- store_v2/inventory/models.py
+++ store_v2/inventory/models.py
@@ -1,3 +1,6 @@
+from distutils.command.upload import upload
+from email.policy import default
+from tabnanny import verbose
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from mptt.models import MPTTModel, TreeForeignKey, TreeManyToManyField
@@ -205,3 +208,42 @@ class ProductInventory(models.Model):
 
     def __str__(self):
         return self.product.name
+
+
+class Media(models.Model):
+    product_inventory = models.ForeignKey(
+        ProductInventory,
+        on_delete=models.PROTECT,
+        related_name="media_product_inventory",
+    )
+    image = models.ImageField(
+        verbose_name=_("product image"),
+        upload_to="images/",
+        default="images/default.png",
+        help_text=_("format: required, default-default.png"),
+    )
+    alt_text = models.CharField(
+        max_length=255,
+        verbose_name=_("alternative text"),
+        help_text=_("format: required, max-255"),
+    )
+    is_feature = models.BooleanField(
+        verbose_name=_("product default image"),
+        help_text=_("format: default=false, true=default image"),
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+        verbose_name=_("date product created"),
+        help_text=_("format: Y-m-d H:M:S"),
+    )
+    updated_at = models.DateTimeField(
+        auto_now_add=True,
+        editable=False,
+        verbose_name=_("date product created"),
+        help_text=_("format: Y-m-d H:M:S"),
+    )
+
+    class Meta:
+        verbose_name = _("product image")
+        verbose_name_plural = _("product images")
