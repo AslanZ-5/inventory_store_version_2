e6f93b3507beaf90710c9822a728b55ad13a0e20
мансур зурабов <asl.zurabov@gmail.com>
2022-03-13 20:24:31 +0300

    filtering product by category

--- store_v2/demo/urls.py
+++ store_v2/demo/urls.py
@@ -6,4 +6,9 @@ app_name = "demo"
 urlpatterns = [
     path("", views.home, name="home"),
     path("categories/", views.category, name="categories"),
+    path(
+        "product-by-category/<slug:category>/",
+        views.product_by_category,
+        name="product_by_category",
+    ),
 ]
--- store_v2/demo/views.py
+++ store_v2/demo/views.py
@@ -10,3 +10,11 @@ def category(request):
     data = models.Category.objects.all()
     print(data)
     return render(request, "categories.html", {"data": data})
+
+
+def product_by_category(request, category):
+    data = models.Product.objects.filter(category__name=category).values(
+        "id", "name", "category__name", "product__store_price"
+    )
+    print(data)
+    return render(request, "product_by_category.html", {"data": data})
