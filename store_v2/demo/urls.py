761bf01b0943be73eb3b7b635115c2cd92ee6f31
мансур зурабов <asl.zurabov@gmail.com>
2022-03-13 19:09:43 +0300

    category page

--- store_v2/demo/urls.py
+++ store_v2/demo/urls.py
@@ -5,4 +5,5 @@ app_name = "demo"
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("categories/", views.category, name="categories"),
 ]
--- store_v2/demo/views.py
+++ store_v2/demo/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from store_v2.inventory import models
 
 
 def home(request):
-    return render(request, "base.html")
+    return render(request, "index.html")
 
 
 def category(request):
-    data = models
+    data = models.Category.objects.all()
+    print(data)
+    return render(request, "categories.html", {"data": data})
